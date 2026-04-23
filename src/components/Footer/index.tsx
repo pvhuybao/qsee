d1fba9e95e6ce0f7533fd3d70bcba9aef387674e
Bao Pham <32448370+pvhuybao@users.noreply.github.com>
2026-04-24 00:54:31 +0700

    Update content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <>
-      <footer className="dark:bg-gray-dark relative z-10 bg-white pt-16 md:pt-20 lg:pt-24">
+      <footer className="dark:bg-gray-dark relative z-10 bg-white pt-8">
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-5/12">
